49ecbaa28807642ca2dcaa21b175c52274d69c3c
Robert Stolarz <robert.stolarz@yandex.com>
2016-07-05 01:32:35 -0400

    receive messages from channels with certain names
    * receive messages via 'message' callback
    * filter out messages that are from channels with different names than
    ones in the list

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ function connect(){
 	client.connect();
 }
 
+function handleMessage(uData, uID, cID, text, e){
+	// I can hear it already: "isn't that inefficient?"
+	// Yes. It is. But channel state might change and I don't want to deal with cache invalidation
+	if(settings.listenChannelNames && settings.listenChannelNames.indexOf(client.channels[cID].name) == -1) return;
+	console.log('"'+text+'"');
+}
+
 client.on('ready',main);
 client.on('disconnect',connect);
+client.on('message',handleMessage);
 // let's get this party started
 connect();
