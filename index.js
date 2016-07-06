94f7ad291f1682cb60260fcf2629ac41bf46b07b
Robert Stolarz <robert.stolarz@yandex.com>
2016-07-06 02:29:00 -0400

    add volume command and player subsystem
    * a player subsystem was added to manage the various players inhabiting
    the channels. this will be used later for queue and dl functionality
    * a volume command has been added with some of the slickest-ass ffmpeg
    wrangling this side of the milky way. get ready to learn something

--- index.js
+++ index.js
@@ -18,12 +18,23 @@ function connect(){
 	client.connect();
 }
 
+var playersByVcID = {};
+
+function isPlaying(vcID){
+	return (playersByVcID[vcID]||{}).isPlaying || false;
+}
+
+function getVolume(vcID){
+	return (playersByVcID[vcID]||{}).volume || (settings.ffmpeg||{}).defaultVolume || 0.15;
+}
+
 function playFile(vcID, filename, then){
 	// TODO: make this configurable
 	var stereo = true;
 	// set up arguments to ffmpeg
 	console.log("playing file " + filename);
-	var ffArgs = ['-i', filename, '-f', 's16le', '-ar', '48000', '-ac', stereo?2:1, 'pipe:1'];
+	var defaultVolume = getVolume(vcID);
+	var ffArgs = ['-i', filename, '-f', 's16le', '-ar', '48000', '-af', 'volume='+defaultVolume, '-ac', stereo?2:1, 'pipe:1', '-stdin'];
 	var ffmpeg = spawn((settings.ffmpeg||{}).binaryLocation || 'ffmpeg', ffArgs, {stdio: ['pipe', 'pipe', 'ignore']});
 	console.log("spawned ffmpeg");
 	client.joinVoiceChannel(vcID, function(){
@@ -33,12 +44,38 @@ function playFile(vcID, filename, then){
 			ffmpeg.stdout.once('readable', function(){
 				console.log("sending contents");
 				context.send(ffmpeg.stdout);
+				// create new player. (sorry this is here, sorry it looks so bad!)
+				playersByVcID[vcID] = playersByVcID[vcID] || {};
+				playersByVcID[vcID].stdin = ffmpeg.stdin;
+				playersByVcID[vcID].volume = defaultVolume;
 			});
+			var reference = playersByVcID[vcID]; // this will always point to the right one ;)
 			ffmpeg.stdout.once('end', function(){
+				reference.isPlaying = false;
 				if(then) return then();
 			});
 		});
 	});
+}
+
+function changeVolume(vcID,args){
+	args = args.trim();
+	var adjust = false;
+	if(args.charAt(0) == '+' || args.charAt(0) == '-'){
+		adjust = args.charAt(0);
+		args = args.substring(1,args.length);
+	}
+	var amount = parseFloat(args);
+	var volume;
+	if(adjust)
+		volume = getVolume(vcID) + amount * (adjust == '+' ? 1 : -1);
+	else
+		volume = amount;
+	volume = Math.max(0,Math.min(1,volume)); // constrain volume to avoid clipping and negatives
+	playersByVcID[vcID].volume = volume;
+	playersByVcID[vcID].stdin.cork();
+	playersByVcID[vcID].stdin.write('cvolume -1 volume '+volume+'\n'); // security warning, don't mess this up
+	playersByVcID[vcID].stdin.uncork();
 }
 
 function handleMessage(uData, uID, cID, text, e){
@@ -52,10 +89,22 @@ function handleMessage(uData, uID, cID, text, e){
 	comName = comName[1];
 	switch(comName){
 		case "summon":
-			client.joinVoiceChannel(client.servers[client.channels[cID].guild_id].members[uID].voice_channel_id);
+			var vcID = client.servers[client.channels[cID].guild_id].members[uID].voice_channel_id;
+			var oldVcID = client.servers[client.channels[cID].guild_id].members[client.id].voice_channel_id;
+			client.joinVoiceChannel(vcID);
+			// if it's switching voice channels, we need to switch the references too
+			// remember: if your code needs to reference its player, it needs to keep a reference and not just use its ID
+			// otherwise I keel u
+			if(playersByVcID[oldVcID]){
+				playersByVcID[vcID] = playersByVcID[oldVcID];
+				delete playersByVcID[oldVcID];
+			}
 			break;
 		case "playfile":
 			playFile(client.servers[client.channels[cID].guild_id].members[client.id].voice_channel_id,comArgs);
+			break;
+		case "volume":
+			changeVolume(client.servers[client.channels[cID].guild_id].members[client.id].voice_channel_id,comArgs);
 			break;
 	}
 }
